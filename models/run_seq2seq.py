36584488ee42d155e20611be832de73c71ace0de
mawilson1234 <michael.a.wilson@yale.edu>
2022-05-06 12:07:29 -0400

    attempt re fix

--- models/run_seq2seq.py
+++ models/run_seq2seq.py
@@ -653,7 +653,7 @@ def main():
 		fig.set_size_inches(8, 6)
 		title = os.path.split(training_args.output_dir)[-1]
 		title = re.findall('(neg-.*)-.*?$', title)[0].replace('-', '_')
-		title = f'training: {title}, test: {re.findall("(neg_.*)_.*?", basename)}'
+		title = f'training: {title}, test: {re.findall("(neg_.*)_.*?", basename)[0]}'
 		fig.suptitle(title)
 		fig.savefig(os.path.join(training_args.output_dir, basename + ".learning_curve.pdf"))
 	
